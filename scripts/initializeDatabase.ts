0dbeee94e029cbca788b3619e45d6722206a71cf
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-07-26 22:12:32 -0300

    added first level try to get all the folders

--- scripts/initializeDatabase.ts
+++ scripts/initializeDatabase.ts
@@ -1,5 +1,6 @@
 import { scanSingleFolder, writeJsonFile } from '../src/services/diskService';
 import dotenv from 'dotenv';
+import { Directory } from '../src/utils/typesDefinition';
 
 const main = () => {
     dotenv.config();
@@ -13,7 +14,17 @@ const main = () => {
 
     const data = scanSingleFolder(initiumIter);
 
-    writeJsonFile(outputFolderPath, [data]);
+    writeJsonFile({ outputFolderPath, data: [data], fileName: 'initium_iter' });
+    writeJsonFile({ outputFolderPath, data: { pending_to_scan: data.sub_directories }, fileName: 'pending_to_scan' });
+
+    const finalResult: Directory[] = [];
+
+    data.sub_directories.forEach(subDir => {
+        const scannedData = scanSingleFolder(subDir);
+        finalResult.push(scannedData);
+    });
+
+    writeJsonFile({ outputFolderPath, data: finalResult, fileName: '1_level_scan' });
 };
 
 if (require.main === module) {
