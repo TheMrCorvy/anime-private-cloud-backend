4d16af9a6ae49857639979b6f2b1ec8326729e66
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-07-27 08:10:02 -0300

    implemented new dynamic excluded parent folders

--- scripts/initializeDatabase.ts
+++ scripts/initializeDatabase.ts
@@ -6,13 +6,14 @@ const main = () => {
     dotenv.config();
     const initiumIter = process.env.INITIAL_PATH || '';
     const outputFolderPath = './db';
+    const excludedParents = process.env.EXCLUDED_PARENTS ? JSON.parse(process.env.EXCLUDED_PARENTS) : [];
 
     if (!initiumIter) {
         console.error('INITIAL_PATH environment variable is not set.');
         return;
     }
 
-    const data = scanSingleFolder(initiumIter);
+    const data = scanSingleFolder(initiumIter, excludedParents);
     const pendingToScan: string[] = data.sub_directories;
 
     const finalResult: Directory[] = [];
@@ -22,7 +23,7 @@ const main = () => {
             const folderToRemoveFromPending = pendingToScan.indexOf(dirPath);
             pendingToScan.splice(folderToRemoveFromPending, 1);
 
-            const scannedData = scanSingleFolder(dirPath);
+            const scannedData = scanSingleFolder(dirPath, excludedParents);
             finalResult.push(scannedData);
             pendingToScan.push(...scannedData.sub_directories);
         });
