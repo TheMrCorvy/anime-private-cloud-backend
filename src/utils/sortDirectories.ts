72d915003d35ad7ccb57123cbdccd4b0c3ff3c25
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-08-06 09:28:53 -0300

    now the sorting function sorts the other way

--- src/utils/sortDirectories.ts
+++ src/utils/sortDirectories.ts
@@ -4,7 +4,9 @@ const normalize = (path: string): string => path.replace(/\\/g, '/');
 
 /**
  * Sort the directories depending on how many levels the parent has
- * The more levels they have, the later they will be on the array
+ * The more levels they have, the sooner they will be on the array
+ * because the loop that will iterate through the directories
+ * will iterate it inverted.
  */
 const sortDirectories = (directories: Directory[]): Directory[] => {
     const pendingToSort = directories.map(dir => ({ ...dir, parent_directory: dir.parent_directory || '' }));
@@ -14,7 +16,7 @@ const sortDirectories = (directories: Directory[]): Directory[] => {
         const normalizedB = normalize(directoryB.parent_directory).split('/').filter(Boolean).length;
 
         if (normalizedA !== normalizedB) {
-            return normalizedA - normalizedB; // Sort by depth first
+            return normalizedB - normalizedA;
         }
 
         return directoryA.parent_directory.localeCompare(directoryB.parent_directory);
