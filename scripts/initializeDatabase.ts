c0a76d21564f2145b51ce29c8f153848f2249061
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-08-09 13:29:58 -0300

    now the script works completely finegit add .

--- scripts/initializeDatabase.ts
+++ scripts/initializeDatabase.ts
@@ -1,17 +1,17 @@
 import { scanSingleFolder, writeJsonFile } from '../src/services/diskService';
 import dotenv from 'dotenv';
-import { AnimeEpisodeResponseStrapi, Directory, DirectoryResponseStrapi } from '../src/utils/typesDefinition';
+import { Directory, DirectoryResponseStrapi } from '../src/utils/typesDefinition';
 
 import sortDirectories from '../src/utils/sortDirectories';
 
 import {
     getAllDirectories,
-    uploadDirectory,
     uploadBulkAnimeEpisodes,
     DirectoryUpdate,
     updateDirectory,
+    uploadDirectoryBulk,
 } from '../src/services/strapiService';
-import fakeApiCall from '../mock/mockApiCall';
+import separateArrays from '../src/utils/separateArrays';
 
 const main = async () => {
     dotenv.config();
@@ -105,6 +105,123 @@ const main = async () => {
     console.log(' ');
 
     const pendingDirectories = sortDirectories([...filteredDirectories]);
+    const separatedPendingDirectories = separateArrays(pendingDirectories, 50);
+
+    for (const directoryChunk of separatedPendingDirectories) {
+        console.log(
+            'Uploading chunks of directories to Strapi...',
+            directoryChunk.map(dir => dir.display_name)
+        );
+
+        await uploadDirectoryBulk(directoryChunk);
+    }
+
+    console.log(' ');
+    console.log('- - - - - - - - - - - - -');
+    console.log('Uploaded all directories! Now uploading anime episodes and updating directories...');
+    console.log('- - - - - - - - - - - - -');
+    console.log(' ');
+
+    const updatedStrapidata = await getAllDirectories();
+    const failedDirectories: Directory[] = [];
+
+    for (const directoryPendingToUpdate of pendingDirectories) {
+        const strapiDirectory = updatedStrapidata.find(
+            dir => dir.directory_path === directoryPendingToUpdate.directory_path
+        );
+
+        if (!strapiDirectory) {
+            console.warn(`Directory ${directoryPendingToUpdate.display_name} not found in Strapi. Skipping update.`);
+
+            failedDirectories.push(directoryPendingToUpdate);
+            continue;
+        }
+
+        const separatedAnimeEpisodes = separateArrays(directoryPendingToUpdate.anime_episodes, 50);
+        const uploadedAnimeEpisodesIds: number[] = [];
+        const patch: DirectoryUpdate = {
+            id: strapiDirectory.id,
+            directoryDocumentId: strapiDirectory.documentId,
+            display_name: directoryPendingToUpdate.display_name,
+        };
+
+        for (const animeEpisodesChunk of separatedAnimeEpisodes) {
+            console.log('Uploading chunk of anime episodes', {
+                parent: directoryPendingToUpdate.display_name,
+                animeEpisodesChunk: animeEpisodesChunk.map(ep => ep.display_name),
+            });
+            console.log(' ');
+
+            const response = await uploadBulkAnimeEpisodes(animeEpisodesChunk, strapiDirectory.id);
+            uploadedAnimeEpisodesIds.push(...response.map(ep => ep.id));
+
+            console.log(' ');
+        }
+
+        if (directoryPendingToUpdate.anime_episodes.length > 0) {
+            patch.anime_episodes = uploadedAnimeEpisodesIds;
+        }
+
+        if (directoryPendingToUpdate.parent_directory) {
+            const parentDirectory = updatedStrapidata.find(
+                dir => dir.directory_path === directoryPendingToUpdate.parent_directory
+            );
+
+            if (!parentDirectory) {
+                console.warn(
+                    `Parent directory ${directoryPendingToUpdate.parent_directory} not found in Strapi. Skipping update.`
+                );
+
+                failedDirectories.push(directoryPendingToUpdate);
+                continue;
+            }
+
+            patch.parent_directory = parentDirectory.id;
+        }
+
+        if (directoryPendingToUpdate.sub_directories.length > 0) {
+            const subDirectories = updatedStrapidata
+                .filter(dir => directoryPendingToUpdate.sub_directories.includes(dir.directory_path))
+                .map(dir => dir.id);
+
+            if (subDirectories.length !== directoryPendingToUpdate.sub_directories.length) {
+                console.warn(
+                    `Subdirectories for ${directoryPendingToUpdate.display_name} not found in Strapi. Skipping update.`
+                );
+                failedDirectories.push(directoryPendingToUpdate);
+                continue;
+            }
+
+            patch.sub_directories = subDirectories;
+        }
+
+        console.log('Updating directory in Strapi', {
+            directory: directoryPendingToUpdate.display_name,
+            patch,
+        });
+        console.log(' ');
+        await updateDirectory(patch);
+    }
+
+    if (failedDirectories.length > 0) {
+        console.warn(
+            'Some directories failed to update:',
+            failedDirectories.map(dir => dir.display_name)
+        );
+        console.warn('You may need to check these directories manually.');
+
+        writeJsonFile({
+            outputFolderPath,
+            data: failedDirectories,
+            fileName: 'failed_directories',
+        });
+    }
+
+    console.log(' ');
+    console.log('- - - - - - - - - - - - -');
+    console.log('Database is fully operative!');
+    console.log('- - - - - - - - - - - - -');
+    console.log(' ');
 };
 
 if (require.main === module) {
